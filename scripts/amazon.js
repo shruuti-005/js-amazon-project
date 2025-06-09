29a63aa55760ad750bd09c7d7a44988711456c1c
shruuti-005 <shrutidasari6@gmail.com>
2025-06-09 12:41:52 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,8 +70,14 @@ let productsHTML = '';
           quantity: 1
         });
       }
+
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
       
-      console.log(cart);
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      
     });
   });
   
