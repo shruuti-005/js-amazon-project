4dc859bd189f2a142d3813839214e031a8795166
shruuti-005 <shrutidasari6@gmail.com>
2025-06-12 19:41:39 +0530

    delete products from when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML += 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -96,12 +96,15 @@ cart.forEach((cartItem) => {
     `
 
     document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-    console.log(cartSummaryHTML);
 
     document.querySelectorAll('.js-delete-link').forEach((link) => {
+        
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
     });
 });
