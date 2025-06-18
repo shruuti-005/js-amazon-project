a3a5966aaf9777e1fdb65aaeadc04c19affceead
shruuti-005 <shrutidasari6@gmail.com>
2025-06-18 17:22:06 +0530

    Save data and generate html for payment summary

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,5 @@
 export function formatCurrency(priceCents){
-    return (priceCents / 100).toFixed(2);  
+    return (Math.round(priceCents) / 100).toFixed(2);  
 }
 
 // Default export can use only once, Each file can only have 1 default export
